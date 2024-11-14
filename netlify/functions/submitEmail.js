ee76e4eeaca5f37f37d9f1d4d186a35f27577ba4
Rahul Malhotra <78801955+ssecurityy@users.noreply.github.com>
2024-11-14 22:42:34 +0530

    pp

--- netlify/functions/submitEmail.js
+++ netlify/functions/submitEmail.js
@@ -4,44 +4,43 @@ let client;
 const uri = process.env.MONGODB_URI;
 
 exports.handler = async function(event) {
-    if (event.httpMethod !== 'POST') {
-        return {
-            statusCode: 405,
-            body: JSON.stringify({ message: 'Method Not Allowed' }),
-        };
+  if (event.httpMethod !== 'POST') {
+    return {
+      statusCode: 405,
+      body: JSON.stringify({ message: 'Method Not Allowed' }),
+    };
+  }
+
+  const { email } = JSON.parse(event.body);
+
+  if (!email) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({ message: 'Email is required' }),
+    };
+  }
+
+  try {
+    if (!client) {
+      client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+      await client.connect();
     }
 
-    const { email } = JSON.parse(event.body);
+    const database = client.db('maubank');
+    const collection = database.collection('emails');
 
-    if (!email) {
-        return {
-            statusCode: 400,
-            body: JSON.stringify({ message: 'Email is required' }),
-        };
-    }
+    await collection.insertOne({ email: email, timestamp: new Date() });
 
-    try {
-        if (!client) {
-            client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-            await client.connect();
-        }
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: 'Email submitted successfully!' }),
+    };
+  } catch (error) {
+    console.error('Error storing email:', error);
 
-        const database = client.db('maubank');
-        const collection = database.collection('emails');
-
-        await collection.insertOne({ email: email, timestamp: new Date() });
-
-        return {
-            statusCode: 200,
-            body: JSON.stringify({ message: 'Email submitted successfully!' }),
-        };
-    } catch (error) {
-        // Log specific error details for better debugging
-        console.error('Detailed error:', error); 
-
-        return {
-            statusCode: 500,
-            body: JSON.stringify({ message: 'Internal Server Error', details: error.message }),
-        };
-    }
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: 'Internal Server Error', details: error.message }),
+    };
+  }
 };
